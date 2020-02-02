2170f07cd157f1da909a3786d2872d4535113879
joseph.weidinger <joseph.weidinger@equipmentshare.com>
2020-02-02 09:51:08 -0600

    get prism styles working

--- src/renderer/forms/code-editor.tsx
+++ src/renderer/forms/code-editor.tsx
@@ -3,6 +3,8 @@ import Editor from "react-simple-code-editor";
 import { highlight, languages } from "prismjs/components/prism-core";
 import "prismjs/components/prism-clike";
 import "prismjs/components/prism-javascript";
+import "prismjs/components/prism-applescript";
+import "prismjs/components/prism-bash";
 
 export default ({ input: { onChange, value } }: any) => (
   <Editor
