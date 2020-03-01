99740de51f35934038bb88f6a1ad87153ba18cc2
joseph.weidinger <joseph.weidinger@equipmentshare.com>
2020-03-01 17:01:58 -0600

    more feature

--- src/main/execution/index.ts
+++ src/main/execution/index.ts
@@ -1,4 +1,5 @@
-import { ipcMain } from "electron";
+import { ipcMain, dialog } from "electron";
+import { Either, pipe, Fn } from "@grapheng/prelude";
 
 import * as Types from "../../shared/types";
 import * as AppleScript from "./applescript";
@@ -11,20 +12,33 @@ ipcMain.on("execute", async (event: any, data: Types.Execution.Data) => {
   event.sender.send("execution-result", null, result);
 });
 
-export const execute = (data: Types.Execution.Data): Promise<any> => {
-  switch (data.type) {
-    case Types.Execution.Type.AppleScript:
-      return AppleScript.run(data);
-    case Types.Execution.Type.ShellScript:
-      return ShellScript.run(data);
-    case Types.Execution.Type.JXA:
-      return JXA.run(data);
-    case Types.Execution.Type.KeyboardMaestro:
-      return KeyboardMaestro.run(data);
-    default:
-      console.error(
-        `Unable to execute script because ${data} doesn't have a handler.`
-      );
-      return Promise.resolve(null);
-  }
-};
+const displayError = (error: any) =>
+  pipe(
+    Either.tryCatchError(() => JSON.stringify(error, null, 2)),
+    Either.fold(() => error, Fn.identity),
+    detail =>
+      dialog.showMessageBox({
+        type: "error",
+        buttons: ["OK"],
+        message: "Error executing script.",
+        detail: detail
+      })
+  );
+
+export const execute = (data: Types.Execution.Data): Promise<any> =>
+  (() => {
+    switch (data.type) {
+      case Types.Execution.Type.AppleScript:
+        return AppleScript.run(data);
+      case Types.Execution.Type.ShellScript:
+        return ShellScript.run(data);
+      case Types.Execution.Type.JXA:
+        return JXA.run(data);
+      case Types.Execution.Type.KeyboardMaestro:
+        return KeyboardMaestro.run(data);
+      default:
+        return Promise.reject(
+          `Unable to execute script because ${data} doesn't have a handler.`
+        );
+    }
+  })().catch(displayError);
